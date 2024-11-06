80f10cbaca105e861b6aff6f8b90f2b3ccc01e36
kdowd <kevin.dowd@gmail.com>
2024-11-06 21:51:56 +1300

    url

--- ImGui_Build.cpp
+++ ImGui_Build.cpp
@@ -8,10 +8,13 @@
 
 int main()
 {
+	//https://www.quora.com/Which-rendering-backend-is-best-for-imgui-in-cpp
 	IMGUI_CHECKVERSION();
 	ImGui::CreateContext();
 	ImGuiIO& io = ImGui::GetIO();
 	ImGui::StyleColorsDark();
+
+
 
 	std::cout << "Hello World!\n";
 }
